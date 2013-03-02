d8aeeb9b361c4484f10fa97b7a2268415eb0eac0
ovidiaconescu <ovidiu@ovidiudiaconescu.com>
2013-03-02 17:42:24 +0200

    Updated assesmbly versions to 1.2.0
    This version is pushed to Nuget as 1.2.0 of the
    ObservableCollectionExtended package.
    Includes separate support for .net 4.0 beside 4.5 and all the other
    frameworks in the portable class library

--- ObservableCollectionEx/ObservableCollectionEx.net40/Properties/AssemblyInfo.cs
+++ ObservableCollectionEx/ObservableCollectionEx.net40/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ObservableCollectionEx")]
+[assembly: AssemblyTitle("ObservableCollectionEx.net40")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ObservableCollectionEx")]
+[assembly: AssemblyProduct("ObservableCollectionEx.net40")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("551daf55-e90d-4a1e-bdad-0f40867ec7a3")]
 
 // Version information for an assembly consists of the following four values:
 //
