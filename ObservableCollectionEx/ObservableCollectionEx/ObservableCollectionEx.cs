39b84d744a5eeb276d554ecdbaf602cf60168aa0
ovidiaconescu <ovidiu@ovidiudiaconescu.com>
2013-03-02 16:27:13 +0200

    Created the two classes

---
+++ ObservableCollectionEx/ObservableCollectionEx/ObservableCollectionEx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace ObservableCollectionEx
+{
+    public class ObservableCollectionEx<T> : ObservableCollection<T> where T : INotifyPropertyChanged
+    {
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            Unsubscribe(e.OldItems);
+            Subscribe(e.NewItems);
+            base.OnCollectionChanged(e);
+        }
+
+        protected override void ClearItems()
+        {
+            foreach (T element in this)
+                element.PropertyChanged -= ContainedElementChanged;
+
+            base.ClearItems();
+        }
+
+        private void Subscribe(IList iList)
+        {
+            if (iList != null)
+            {
+                foreach (T element in iList)
+                    element.PropertyChanged += ContainedElementChanged;
+            }
+        }
+
+        private void Unsubscribe(IList iList)
+        {
+            if (iList != null)
+            {
+                foreach (T element in iList)
+                    element.PropertyChanged -= ContainedElementChanged;
+            }
+        }
+
+        private void ContainedElementChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var ex = new PropertyChangedEventArgsEx(e.PropertyName, sender);
+            OnPropertyChanged(ex);
+        }
+    }
+}
---
+++ ObservableCollectionEx/ObservableCollectionEx/PropertyChangedEventArgsEx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableCollectionEx
+{
+    public class PropertyChangedEventArgsEx : PropertyChangedEventArgs
+    {
+        public object Sender { get; private set; }
+
+        public PropertyChangedEventArgsEx(string propertyName, object sender)
+            : base(propertyName)
+        {
+            this.Sender = sender;
+        }
+    }
+}
