28540a6c21247fba1127bafca90e4cd747e093a0
ovidiaconescu <ovidiu@ovidiudiaconescu.com>
2013-03-02 16:41:47 +0200

    Created some unit tests to verify the code

---
+++ ObservableCollectionEx/ObservableCollectionEx.Tests/Model/Element.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableCollectionEx.Tests.Model
+{
+    class Element : INotifyPropertyChanged 
+    {
+        public int Id { get; set; }
+
+        string _value { get; set; }
+        public string Value
+        {
+            get { return _value; }
+            set { _value = value; onPropertyChanged(this, "Value"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void onPropertyChanged(object sender, string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                PropertyChanged(sender, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ ObservableCollectionEx/ObservableCollectionEx.Tests/ObservableCollectionExTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObservableCollectionEx.Tests.Model;
+
+namespace ObservableCollectionEx.Tests
+{
+    [TestClass]
+    public class ObservableCollectionExTests
+    {
+        ObservableCollectionEx.ObservableCollectionEx<Element> col;
+
+        [TestInitialize]
+        public void CreateTheObservableCollectionForTracking()
+        {
+            col = new ObservableCollectionEx<Element>();
+            col.Add(new Element() { Id = 0, Value = "elem 0" });
+        }
+
+        [TestMethod]
+        public void ValuePropertyChangedEventIsRaised_WhenTheValueChanges()
+        {
+            List<string> receivedEvents = new List<string>();
+            ((INotifyPropertyChanged)col).PropertyChanged += delegate(object sender, PropertyChangedEventArgs e)
+            {
+                receivedEvents.Add(e.PropertyName);
+            };
+            col[0].Value = "new elem 0";
+            Assert.AreEqual(1, receivedEvents.Count);
+            Assert.AreEqual("Value", receivedEvents[0]);
+        }
+
+        [TestMethod]
+        public void ValuePropertyChangedEventRaised1000Times_CheckConsistencyOfEvents()
+        {
+            int count = 1000;
+            List<string> receivedEvents = new List<string>();
+            ((INotifyPropertyChanged)col).PropertyChanged += delegate(object sender, PropertyChangedEventArgs e)
+            {
+                receivedEvents.Add(e.PropertyName);
+            };
+
+            for (int i = 0; i < count; i++)
+                col[0].Value = string.Format("elem {0}", i);
+
+            Assert.AreEqual(count, receivedEvents.Count);
+        }
+    }
+}
